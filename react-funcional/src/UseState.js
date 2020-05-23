71b0fb1a35266e8c13daf34943f6c99607338594
unknown <malbuquerquedesouza@gmail.com>
2020-05-23 02:49:59 -0300

    react funcional

---
+++ react-funcional/src/UseState.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+
+function UseState() {
+//declarando as variaveis
+  const [ numero, setNumero ] = useState()
+  const [ segundoNumero, setSegundoNumero ] = useState()
+  const [ resultado, setResultado ] = useState()
+
+  const somar = () => {
+    const numeroInt = parseInt(numero)
+    const segNumeroInt = parseInt(segundoNumero)
+
+    setResultado( numeroInt + segNumeroInt )
+  }
+
+
+  return (
+
+       
+    <div>
+
+Número 1:<br />
+      <input type="text" value={numero} 
+             onChange={e => setNumero(e.target.value)} /><br />
+
+
+      Número 2:<br />
+      <input type="text" value={segundoNumero}
+              onChange={e => setSegundoNumero(e.target.value)} /><br />
+
+
+      <button onClick={somar}>Somar</button> <br />
+      
+      Resultado:<br />
+      <input type="text" value={resultado} /><br />
+    </div>
+  );
+}
+
+export default App;
+
+
+/*outra forma de declarar as variaveis
+const [ state, setState] = useState({
+  numero: 0,
+  segundoNumero: 0,
+  resultado: 0
+})*/
